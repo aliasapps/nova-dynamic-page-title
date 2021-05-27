64c4ca13334d669c83048446789e038152aa0120
Aaron Tagadtad <aaron@alias.com>
2021-05-27 13:06:10 -0400

    typo

--- resources/js/asset.js
+++ resources/js/asset.js
@@ -65,16 +65,16 @@ Nova.booting((Vue, router, store) => {
     if (from.name === "unassigned-orders") {
       sideLabel = document.getElementById("unassigned-orders");
 
-      //   sideTitle = document.querySelectorAll('[dusk="order-index-component"]');
-      //   console.log("sideTitle: ", sideTitle);
-      //   if (sideTitle.length > 0) {
-      //     let h1 = sideTitle[0].getElementsByTagName("h1");
-      //     console.log("h1: ", h1);
-      //     if (h1.length > 0) {
-      //       h1[0].innerHTML = "Unassigned Orders";
-      //       console.log("innerHTML: ", (h1[0].innerHTML = "Unassigned Orders"));
-      //     }
-      //   }
+      sideTitle = document.querySelectorAll('[dusk="orders-index-component"]');
+      console.log("sideTitle: ", sideTitle);
+      if (sideTitle.length > 0) {
+        let h1 = sideTitle[0].getElementsByTagName("h1");
+        console.log("h1: ", h1);
+        if (h1.length > 0) {
+          h1[0].innerHTML = "Unassigned Orders";
+          console.log("innerHTML: ", (h1[0].innerHTML = "Unassigned Orders"));
+        }
+      }
     } else if (from.name === "orders-in-progress") {
       sideLabel = document.getElementById("orders-in-progress");
     } else if (from.name === "orders-completed") {
